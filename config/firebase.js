9c244d918d232fcd566f0b65597bdb6bce9c073c
MarxLaw <96363263+MarxLaw@users.noreply.github.com>
2026-02-09 12:24:02 +0800

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,8 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+//const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
